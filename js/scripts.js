3959a2894b05ad048c5d21ccedde4ff7c4365e21
Juliet-Ol <agutjuliet@gmail.com>
2021-12-22 22:03:29 +0300

    add toggle effect for what we do section

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+$("button").click(function() {
+    $(".dev_icon").toggle();
+    $("design_icon").toggle();
+    $("product").toggle();
+    
+    }); 
+    // $("button").click(function() {
+    //   $("dev_icon").show();
+    // });
+});
+
+// $(document).ready(function() {
+//     $("button").click(function(){
+//         $("design_icon").toggle();
+
+//     });
+// });
+
+// $(document).ready(function() {
+//     $("button").click(function(){
+//         $("product").toggle();
+//     });
+// });
