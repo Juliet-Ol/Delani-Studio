5fe76ca4f8aa6c79e661da8a1a672a7fabb0ea79
Juliet-Ol <agutjuliet@gmail.com>
2021-12-26 02:06:29 +0300

    resize portfolio images

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,30 @@ $(document).ready(function() {
 
 $("button").click(function() {
     $(".dev_icon").toggle();
+      $(p).toggle()
 
     $("design_icon").toggle();
-    $("product").toggle();
+    // $("product").toggle();
     
     }); 
-    // $("button").click(function() {
-    //   $("dev_icon").show();
-    // });
+
+
+    
 });
 
 // $(document).ready(function() {
-//     $("button").click(function(){
-//         $("design_icon").toggle();
+//     const newLocal = $("button").click(function () {
+//     $("product").toggle();
+// });
 
 //     });
 // });
 
-// $(document).ready(function() {
-//     $("button").click(function(){
-//         $("product").toggle();
-//     });
-// });
+$(document).ready(function() {
+    $("button").onClick(function()cancelIdleCallback {
+        $("product").toggle();
+    });
+});
+
+
+
