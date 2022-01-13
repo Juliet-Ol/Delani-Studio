786d72538ef595743f07124f3fec0dcf4a6b2405
Juliet-Ol <agutjuliet@gmail.com>
2022-01-13 11:46:19 +0300

    add toggle on what we do

--- js/scripts.js
+++ js/scripts.js
@@ -1,82 +1,32 @@
 
 $(document).ready(function() {
-  $("button").click(function() {
-    
-      $(".dev_icon").toggle();
-  $("button").click(function(event) {
-      event.preventDefault();
-      $(".dev").toggle();
-        $("p").toggle()
 
-  });
-
+    $(".click").click(function() {
+        $(this).children('.content').toggleClass('active');
+        $(this).children('img').toggleClass('inactive');
+        $(this).children('h3').toggleClass('inactive');
+    });
 });
 
+    function show(){
+      var Name = document.getElementById("Name")
+      var Email = document.getElementById("Email")
+      var yourMessage = document.getElementById("message")
+      var submit = document.getElementById(alert)
 
+      if (Name == " ")
+      show("enter name")
 
-(function($) {
-(document).ready(function() {
-  $("button").click(function() {
-      $(".design_icon").toggle();
-  $("button").click(function() {
-      $(this).closest(".design_icon").toggleClass(active);
-      $(this).closest(".design_icon").find(".design_icon").stop().toggle;
+      else if (Email == " ") 
+        show("enter email")
 
-  });
+        else if (yourMessage == " ") 
+          show("your message")
+      
+        
+        }; 
 
-});
-
-});
-});
-
-(function($){
-(document).ready(function() {
-  $("button").click(function() {
-      $(".product").toggle();
-  $("button").click(function() {      
-      $(".product").toggle();
-        $("p").toggle()
-
-  });
-
-});
-
-});
-
-});
-
-});
-
-
-
-
-  
-function show(){
-  var Name = document.getElementById("Name")
-  var Email = document.getElementById("Email")
-  var yourMessage = document.getElementById("message")
-  var submit = document.getElementById(alert)
-
-  if (Name == " ")
-   show("enter name")
-
-   else if (Email == " ") 
-    show("enter email")
-
-    else if (yourMessage == " ") 
-      show("your message")
-  
-    
-    }; 
-    
-    
+        
       $("#submitForm").click(function() {
         alert("'we have received your message. Thank you for reaching out to us'.");
       });
-
-
-
-      
-
-
- 
